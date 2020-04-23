8d80ba570542c7b7272f7123388bc7856312fb44
Ralph Rudd <contact@ralphrudd.com>
2020-04-23 08:20:06 +0200

    Add FDM for BS Put with test.

--- finite_difference/products.py
+++ finite_difference/products.py
@@ -17,3 +17,21 @@ class FDEuropeanCallOption():
     
     def terminal_condition(self, stock, time):
         return np.maximum(stock - self.strike, 0)
+
+class FDEuropeanPutOption():
+
+    def __init__(self, strike, maturity, short_rate):
+        self.strike = strike
+        self.maturity = maturity
+        self.short_rate = short_rate
+        # TODO: It's a problem that the short rate must be here and in model
+    
+    def lower_boundary(self, stock_min, time):
+        return  -stock_min + self.strike * np.exp(-self.short_rate 
+                * (self.maturity - time))
+
+    def upper_boundary(self, stock_max, time):
+        return 0 * time
+    
+    def terminal_condition(self, stock, time):
+        return np.maximum(self.strike - stock, 0)
--- tests/test_finite_difference.py
+++ tests/test_finite_difference.py
@@ -23,11 +23,42 @@ class Test1DThetaMethod(unittest.TestCase):
         solver = fd.solvers.FD1DThetaMethod(model, product)
         U, S = solver.solve()
 
-        lower_bound = 90
-        upper_bound = 110
+        lower_bound = 45
+        upper_bound = 55
         logical_index = np.logical_and(S >= lower_bound, S <= upper_bound)
         stock = S[logical_index]
         analytical_price = analytical.pricing.price_call_BS(stock, strike, maturity, 
+                short_rate, volatility)
+        fd_price = U[logical_index]
+
+        for i in range(len(stock)):
+            self.assertAlmostEqual(fd_price[i], analytical_price[i],
+                    places=2)
+
+    def test_price_put_BS(self):
+
+        volatility = 0.4
+        short_rate = 0.06
+        time_steps = 40
+        stock_steps = 80
+        stock_min = 0
+        stock_max = 150
+        strike = 50
+        maturity = 1
+        
+        model = fd.models.FDBlackScholesModel(volatility, short_rate, 
+        (stock_min, stock_max), stock_steps, time_steps)
+
+        product = fd.products.FDEuropeanPutOption(strike, maturity, short_rate)
+
+        solver = fd.solvers.FD1DThetaMethod(model, product)
+        U, S = solver.solve()
+
+        lower_bound = 45
+        upper_bound = 55
+        logical_index = np.logical_and(S >= lower_bound, S <= upper_bound)
+        stock = S[logical_index]
+        analytical_price = analytical.pricing.price_put_BS(stock, strike, maturity, 
                 short_rate, volatility)
         fd_price = U[logical_index]
 
